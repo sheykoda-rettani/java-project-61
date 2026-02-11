614d7216bac31b704a7a64b1a37e2c7e49d16fde
Dmitrii Sheiko <sheykoda@gmail.com>
2026-02-11 13:20:03 +0300

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    id("groovy")
+    id("com.github.ben-manes.versions") version "0.53.0"
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.apache.groovy:groovy:5.0.0")
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
